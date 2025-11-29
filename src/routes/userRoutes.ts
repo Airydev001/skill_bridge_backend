0ae0e928967d8a8deb9157514e8c2676f4275d66
charles1234G <charlesgreatness@gmail.com>
2025-11-29 09:20:02 +0100

    changes fixed

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
-import { getUserProfile, updateMentorProfile, updateMenteeProfile } from '../controllers/userController';
+import {
+    getUserProfile,
+    updateMentorProfile,
+    updateMenteeProfile,
+    updateProfile
+} from '../controllers/userController';
 import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateProfile);
 router.post('/mentor-profile', protect, updateMentorProfile);
 router.post('/mentee-profile', protect, updateMenteeProfile);
 
